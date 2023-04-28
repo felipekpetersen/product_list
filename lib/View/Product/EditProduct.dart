db57dfed5b4763dbbcc9524b45657f894bb5321e
Felipe Petersen <felipekpetersen@gmail.com>
2023-04-28 13:36:57 -0300

    fixes in layout detail edit

--- lib/View/Product/EditProduct.dart
+++ lib/View/Product/EditProduct.dart
@@ -7,6 +7,8 @@ import 'package:product_list/Common/Constants.dart';
 import 'package:product_list/Common/Extensions.dart';
 import 'package:product_list/Cubit/Product/cubit/edit_product_cubit.dart';
 
+import '../../Domain/Product/ProductModel.dart';
+
 class DetailProduct extends StatefulWidget {
   const DetailProduct({Key? key}) : super(key: key);
 
@@ -28,7 +30,6 @@ class _DetailProductState extends State<DetailProduct> {
             child: CircularProgressIndicator(),
           );
         else if (state is DetailProductLoaded)
-        
           return _buildDetailScreen(context, state);
         else if (state is DetailProductError)
           return Text(state.error);
@@ -40,24 +41,82 @@ class _DetailProductState extends State<DetailProduct> {
   }
 }
 
-Widget _buildDetailScreen(context,DetailProductLoaded state) {
+Widget _buildDetailScreen(context, DetailProductLoaded state) {
   return Center(
-    child: ConstrainedBox(
+    child: Container(
+      width: MediaQuery.of(context).size.width,
+      height: MediaQuery.of(context).size.height,
       constraints: BoxConstraints(maxWidth: 1200, maxHeight: double.infinity),
-      child: Column(
-        children: [
-          Center(
-            child: Image.network(
-              state.product.image!,
-              width: 300,
-              height: 300,
+      child: CustomScrollView(
+        slivers: [
+          SliverFillRemaining(
+            hasScrollBody: false,
+            child: Column(
+              children: [
+                Center(
+                  child: Image.network(
+                    state.product.image!,
+                    width: 300,
+                    height: 300,
+                  ),
+                ),
+                SizedBox(
+                  height: 16,
+                ),
+                buildDetailBox(state.product)
+              ],
             ),
-          ),
-          Text(state.product.title ?? "No Title"),
-          Text((state.product.price ?? 0.0).toPrice()),
-          Text(state.product.description ?? "No Description"),
+          )
         ],
       ),
     ),
+  );
+}
+
+Widget buildDetailBox(ProductsModel product) {
+  return Expanded(
+    child: Container(
+        padding: EdgeInsets.all(16),
+        decoration: BoxDecoration(
+          color: Colors.white,
+          borderRadius: BorderRadius.circular(16),
+        ),
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.start,
+          crossAxisAlignment: CrossAxisAlignment.start,
+          children: [
+            Padding(
+              padding: const EdgeInsets.all(8.0),
+              child: Row(
+                mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                children: [
+                  Text(product.title ?? "No Title"),
+                  Text((product.price ?? 0.0).toPrice()),
+                ],
+              ),
+            ),
+            Padding(
+              padding: const EdgeInsets.all(8.0),
+              child: Text("Sobre"),
+            ),
+            Padding(
+              padding: const EdgeInsets.all(8.0),
+              child: Text(product.description ?? "No Description"),
+            ),
+            Expanded(child: Container(),),
+            Center(
+              child: Container(
+                padding: EdgeInsets.all(16),
+                margin: EdgeInsets.all(16),
+                // height: 16,
+                // width: 16,
+                decoration: BoxDecoration(color: Colors.black,
+                borderRadius: BorderRadius.circular(8)),
+                child: Text("Add to cart", style: TextStyle(color: Colors.white),
+                )
+              ),
+            )
+          ],
+        )),
   );
 }
